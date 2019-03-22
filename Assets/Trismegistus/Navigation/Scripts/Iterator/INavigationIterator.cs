05df994edf4fc9a44f25e18d60c4674a71b6434a
Hermesis <hermesiss@gmail.com>
2019-03-22 19:13:34 +0400

    Add Examples/IteratorExample.cs with resources and visualization (#6)

--- Assets/Trismegistus/Navigation/Scripts/Iterator/INavigationIterator.cs
+++ Assets/Trismegistus/Navigation/Scripts/Iterator/INavigationIterator.cs
@@ -1,13 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Trismegistus.Navigation.Iterator
 {
-    public interface INavigationIterator : IEnumerator<Vector3>
+    public interface INavigationIterator
     {
         float StoppingDistance { get; }
-        Event DestinationChanged { get; }
+        UnityEvent DestinationChanged { get; }
         Vector3 Destination { get; }
         void SetStoppingDistance(float distance);
+        void SetNavigationManager(INavigationManager manager);
+    }
+
+    public interface INavigationIteratorCreator
+    {
+        INavigationIterator GetNavigationIterator(GameObject targetGameObject, float stoppingDistance);
     }
 }
