2c279e8b9834d9890298b2b9648e10ca4dbfdea6
Hermesis <hermesiss@gmail.com>
2019-04-11 11:35:16 +0400

    new chmod

--- Ci/commit2packagebranch.sh
+++ Ci/commit2packagebranch.sh
@@ -2,21 +2,21 @@
 
 echo "git push"
 
-show_tree.sh
+ci/show_tree.sh
 
 git archive -o archive.tar HEAD:Assets/Trismegistus
 
-show_tree.sh
+ci/show_tree.sh
 
 git checkout unity-package-manager
 
-show_tree.sh
+ci/show_tree.sh
 
 tar -xf archive.tar
 rm archive.tar
 git add -A
 
-show_tree.sh
+ci/show_tree.sh
 
 #git config --global user.email "travis@travis-ci.org"
 #git config --global user.name "Travis CI"
